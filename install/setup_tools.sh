a75d16f7cd1c2c257cc8d0c4c1119a882d5f2c2c
Matt Carr <mcarr@kinaxis.com>
2024-07-30 12:47:00 -0400

    Install cheat as part of tools

--- install/setup_tools.sh
+++ install/setup_tools.sh
@@ -4,9 +4,12 @@ sudo apt update -y
 sudo apt upgrade -y
 
 echo "Installing basic packages"
-sudo apt install bash bat ca-certificates curl delta diffutils fd-find findutils fzf gh git git-delta git-lfs jq kdiff3 nodejs npm python3-dev python3-pip python3-setuptools yq
+sudo apt install bash bat ca-certificates cheat curl delta diffutils fd-find findutils fzf gh git git-delta git-lfs jq kdiff3 nodejs npm python3-dev python3-pip python3-setuptools yq
 
-echo "Installing...other stuff"
+echo "Loading cheatsheets"
+rsync -a ../cheat ~/.config/cheat
+
+echo "Installing fuck"
 pip3 install thefuck --user
 
 echo "Installing pyenv"
