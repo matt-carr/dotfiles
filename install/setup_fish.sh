292e58c3eb48313fe7fe8e23cc0c1f1d548d2f22
Matt Carr <matt.carr@uber.com>
2025-07-07 13:43:10 +0000

    update fish to v4, remove gitconfig from fish setup

--- install/setup_fish.sh
+++ install/setup_fish.sh
@@ -27,8 +27,4 @@ fish -c "fisher update"
 # configure tide shell
 fish -c "tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Light --show_time='24-hour format' --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline-prompt-tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No" 
 
-# setup git
-echo "Setting up git"
-cp ../gitconfig ~/.gitconfig
-
 echo "========Fish Setup Complete!========"
