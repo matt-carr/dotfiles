17fcf3db1ffc03d4271e6319541a294592c880b1
matt.carr <matt.carr@uber.com>
2025-10-21 13:59:31 +0000

    Update fish setup to debian 12

--- install/setup_fish.sh
+++ install/setup_fish.sh
@@ -1,9 +1,9 @@
 # install fish
 echo "Adding fish PPA and installing fish"
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
-curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
 sudo apt update
-sudo apt install fish -y
+sudo apt install fish
 sudo usermod -s /usr/bin/fish matt.carr
 confDir=$(fish -c 'echo $__fish_config_dir')
 
