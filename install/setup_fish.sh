7e5e6f149576f69022109709b21e61bc8340a2c4
Matt Carr <matt.carr@uber.com>
2025-10-28 12:28:50 -0400

    add -y to fish install

--- install/setup_fish.sh
+++ install/setup_fish.sh
@@ -3,7 +3,7 @@ echo "Adding fish PPA and installing fish"
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
 sudo apt update
-sudo apt install fish
+sudo apt install fish -y
 sudo usermod -s /usr/bin/fish matt.carr
 confDir=$(fish -c 'echo $__fish_config_dir')
 
